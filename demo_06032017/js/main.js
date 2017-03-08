6c83977c82a924b10a9a15216684737b4d2afaa3
Nguyenxuanvinh309 <nguyenxuanvinh309@gmail.com>
2017-03-08 18:49:06 +0700

    Add files via upload

--- demo_06032017/js/main.js
+++ demo_06032017/js/main.js
@@ -8,4 +8,7 @@ $(window).on('resize', function(){
 
 function SetHeightGrid() {
 	$(".height-col-child").css("height", $(".height-col-main").height());
+	$(".height-col-cself").css("height", $(".height-col-main").height()*2);
+	$(".height-col-ctext").css("height", $(".height-col-ptext").height());
+	$(".height-col-picdouc").css("height", $(".height-col-picdoup").height());
 }
