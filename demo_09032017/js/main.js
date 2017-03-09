ad8717ddc435683bd195be60d0932aef457ee971
Nguyenxuanvinh309 <nguyenxuanvinh309@gmail.com>
2017-03-09 17:43:03 +0700

    Add files via upload

---
+++ demo_09032017/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+      SetHeightGrid();
+});
+
+$(window).on('resize', function(){
+      SetHeightGrid();
+});
+
+function SetHeightGrid() {
+	$(".height-col-child").css("height", $(".height-col-main").height());
+	$(".height-col-cself").css("height", $(".height-col-main").height()*2);
+	$(".height-col-ctext").css("height", $(".height-col-ptext").height());
+	$(".height-col-picdouc").css("height", $(".height-col-picdoup").height());
+
+	/*With Screen < 768px*/
+	if($("body").width() < 768){
+		$(".height-col-child").css("height", $(".height-col-main").height()/4);
+	}
+}
